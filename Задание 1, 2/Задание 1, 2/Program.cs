91dea2f2422956ddb5905db9fa6896729ce6a78b
0-kartOshka-0 <darazajceva3675@mail.ru>
2024-08-17 15:39:33 +0300

    Quests

---
+++ Задание 1, 2/Задание 1, 2/Program.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Задание_1__2
+{
+    class Program
+    {
+        static void Decore()
+        {
+            Console.WriteLine("~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~");
+        }
+
+        static double CalculateArea(double a, double b, double c)
+        {
+            if (a + b > c && a + c > b && b + c > a && a > 0 && b > 0 && c > 0)
+            {
+                double p = (a + b + c) / 2;
+                double s = Math.Sqrt(p * (p - a) * (p - b) * (p - c));
+                return s;
+            }
+            else
+                throw new Exception("Треугольник невозможен");
+        }
+        static string DetermineTriangleType(double a, double b, double c)
+        {
+            if (a + b > c && a + c > b && b + c > a && a > 0 && b > 0 && c > 0)
+            {
+                if (a * a + b * b == c * c || a * a + c * c == b * b || b * b + c * c == a * a)
+                {
+                    return "Прямоугольный";
+                }
+                if (a * a + b * b > c * c && a * a + c * c > b * b && b * b + c * c > a * a)
+                {
+                    return "Остроугольный";
+                }
+                else
+                {
+                    return "Тупоугольный";
+                }
+            }
+            else
+                throw new Exception("Треугольник невозможен");
+        }
+
+
+        static void Main(string[] args)
+        {
+            try
+            {
+                Decore();
+                Console.WriteLine("Введите стороны треугольника:");
+                Console.Write("a: ");
+                double a = double.Parse(Console.ReadLine());
+                Console.Write("b: ");
+                double b = double.Parse(Console.ReadLine());
+                Console.Write("c: ");
+                double c = double.Parse(Console.ReadLine());
+                Decore();
+                double area = CalculateArea(a, b, c);
+                Console.WriteLine("Площадь треугольника: " + area);
+                string type = DetermineTriangleType(a, b, c);
+                Console.WriteLine("Тип треугольника: " + type);
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+                Console.ReadLine();
+                return;
+            }
+            Decore();
+            Console.ReadLine();
+        }
+    }
+}
---
+++ Задание 1, 2/Задание 2/Program.cs
@@ -0,0 +1,104 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Задание_2
+{
+    
+    class Program
+    {
+        static Random random = new Random();
+        static void Decore()
+        {
+            Console.WriteLine("~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~");
+        }
+        static int[] RandomArr(int size) // Функция для генерации случайного массива
+        {
+            
+            int[] array = new int[size];
+            for (int i = 0; i < size; i++)
+            {
+                array[i] = random.Next(1, 25); // Генерация случайного числа от 1 до 100
+            }
+            return array;
+        }
+        static void Arr(int[] array) // Функция для вывода массива на экран
+        {
+            foreach (int num in array)
+            {
+                Console.Write(num + " ");
+            }
+            Console.WriteLine();
+        }
+        static int Sum(int[] array)// Функция для нахождения суммы не кратных 5 элементов массива
+        {
+            int sum = 0;
+            foreach (int num in array)
+            {
+                if (num % 5 != 0)
+                {
+                    sum += num;
+                }
+            }
+            return sum;
+        }
+        static void Swap(ref int[] array)// Функция для обмена первой и второй половин массива
+        {
+            int temp;
+            int mid = array.Length / 2;
+            for (int i = 0; i < mid; i++)
+            {
+                temp = array[i];
+                array[i] = array[mid + i];
+                array[mid + i] = temp;
+            }
+        }
+
+        static void Main(string[] args)
+        {
+            try
+            {
+                Console.Write("Введите размерность первого массива: ");
+                int size1 = int.Parse(Console.ReadLine());
+                Console.Write("Введите размерность второго массива: ");
+                int size2 = int.Parse(Console.ReadLine());
+
+                int[] arr1 = RandomArr(size1);
+                int[] arr2 = RandomArr(size2);
+                Decore();
+                Console.WriteLine("Первый массив:");
+                Arr(arr1);
+                Console.WriteLine("Второй массив:");
+                Arr(arr2);
+                Decore();
+                int sum1 = Sum(arr1);
+                int sum2 = Sum(arr2);
+                Console.WriteLine($"Сумма не кратных 5 элементов первого массива: {sum1}");
+                Console.WriteLine($"Сумма не кратных 5 элементов второго массива: {sum2}");
+                Decore();
+                if (sum1 < sum2)
+                {
+                    Swap(ref arr1);
+                }
+                else
+                {
+                    Swap(ref arr2);
+                }
+                Console.WriteLine("Первый массив после обмена:");
+                Arr(arr1);
+                Console.WriteLine("Второй массив после обмена:");
+                Arr(arr2);
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+                Console.ReadLine();
+                return;
+            }
+            Decore();
+            Console.ReadLine();
+        }
+    }
+}
